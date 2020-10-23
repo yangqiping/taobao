5f4c5bc3d66bcde10cf0821160c5f22cdd3297ee
yangqiping <1752048561@qq.com>
2020-10-23 15:40:54 +0800

    1023

---
+++ build/precache-manifest.698300b6975ffaec17c2286bab05c3b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accc2966202957238843401d613f09ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f85df35c7d23fdfb0c52",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "66878061dbea062d0ec7",
+    "url": "/static/css/main.781b88b1.chunk.css"
+  },
+  {
+    "revision": "f85df35c7d23fdfb0c52",
+    "url": "/static/js/2.83d5eab8.chunk.js"
+  },
+  {
+    "revision": "de5fa9cfc3624bea3d7565f048bfaaaf",
+    "url": "/static/js/2.83d5eab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66878061dbea062d0ec7",
+    "url": "/static/js/main.2ee7cedc.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "6b55396ec634fbe26567a34c9751831f",
+    "url": "/static/media/taobao.6b55396e.PNG"
+  }
+]);
